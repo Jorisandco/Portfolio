d0b193c73e58b6407a0d05d0abdef6bf888daed1
Joris van Rijn <jorisvanrijn@outlook.com>
2026-09-10 17:54:55 +0200

    formated document

--- src/main.js
+++ src/main.js
@@ -1,24 +1,23 @@
 import './style.css'
 import { NavHandler } from './classes/NavHandler.js'
-import { Aboutme } from './PageLoaders/AboutMe.js';
-import { Projects } from './PageLoaders/Projects.js';
+import { Aboutme } from './PageLoaders/AboutMe.js'
+import { Projects } from './PageLoaders/Projects.js'
 
 const Navigation = new NavHandler()
 const pages = {
   aboutme: Aboutme,
-  myProjects: Projects,
-};
+  myProjects: Projects
+}
 
-const currentPoint = Navigation.GetCurrentPoint();
+const currentPoint = Navigation.GetCurrentPoint()
 
 if (pages[currentPoint]) {
-  const PageClass = pages[currentPoint];
-  const pageInstance = new PageClass();
-  
-  pageInstance.LoadPage();
-}
-else{
-        document.querySelector('#app').innerHTML = `
+  const PageClass = pages[currentPoint]
+  const pageInstance = new PageClass()
+
+  pageInstance.LoadPage()
+} else {
+  document.querySelector('#app').innerHTML = `
         <div id="notfoundError">404 this page doesn't exsist</div>
     `
 }
