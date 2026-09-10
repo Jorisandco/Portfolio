07bf0d079cc67d89f2187f41dfe7702162a147fd
Joris van Rijn <jorisvanrijn@outlook.com>
2026-09-10 18:02:26 +0200

    changed how Nav handler handles getting ObjectID which is meant for like getting the right project to display

--- src/classes/NavHandler.js
+++ src/classes/NavHandler.js
@@ -1,34 +1,40 @@
 export class NavHandler {
   constructor () {
-    this.CurrentLink = window.location.href
-    this.protocol = window.location.protocol
-    this.Linkorigin = window.location.origin
-    this.PageObjectID = null
+    this.CurrentLink = window.location.href;
+    this.protocol = window.location.protocol;
+    this.Linkorigin = window.location.origin;
 
-    console.log(window.location)
+    console.log(window.location);
   }
 
-  switchPage (page) {
-    let params = new URLSearchParams(document.location.search)
+  switchPage (page, PageObjectID = null) {
+    let params = new URLSearchParams(document.location.search);
+    params.set('page', page);
 
-    params.set('page', page)
-    window.history.pushState({}, '', `?${params.toString()}`)
+    if (PageObjectID !== null) params.set('ObjectID', PageObjectID);
+
+    window.history.pushState({}, '', `?${params.toString()}`);
   }
 
   GetCurrentPoint () {
-    this.CheckifCurrentPointIsSet()
+    this.CheckifCurrentPointIsSet();
 
-    let params = new URLSearchParams(document.location.search)
+    const params = new URLSearchParams(document.location.search);
 
-    return params.get("page")
+    return params.get('page');
   }
 
-  GetObjectID () {}
+  GetObjectID () {
+    const params = new URLSearchParams(document.location.search);
+
+    return params.get('ObjectID');
+  }
 
   CheckifCurrentPointIsSet () {
     // Instead of manually splitting, use URLSearchParams consistently
     const nopageParam =
       new URLSearchParams(window.location.search).get('page') === null
+
     if (nopageParam === true) this.switchPage('aboutme')
   }
 }
