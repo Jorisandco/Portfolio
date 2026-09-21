c2e5510212687420620620026f94c5e675d90839
Joris van Rijn <jorisvanrijn@outlook.com>
2026-09-21 14:11:41 +0200

    added logic that will later be changed to account for api to show page numbers

--- src/javascript/PageLoaders/Projects.js
+++ src/javascript/PageLoaders/Projects.js
@@ -5,6 +5,7 @@ export class Projects extends PageLoaderBase {
     constructor(nav) {
         super(nav);
         this.currentPage = "myProjects"
+        this.currentPageCount = 2
     }
 
     LoadPage() {
@@ -18,6 +19,14 @@ export class Projects extends PageLoaderBase {
                 </h1>
                 <div id="random-selection">
                     ${this.loadRandomProjects()}
+                </div>
+                <div id="allProjects">
+                    ${this.showAllProjects()}
+                    <div class="pagination">
+                        <div class="page-number" id="page-down"><</div>
+                        ${this.loadPageCount()}
+                        <div class="page-number" id="page-up">\></div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -39,6 +48,47 @@ export class Projects extends PageLoaderBase {
         results.forEach((result) => {
             html += `<div class="project-element"><img src="${result.image}"> <div class="name-container">${result.name}</div></div>`
         })
+
+        return html
+    }
+
+    showAllProjects() {
+        let html = "";
+        const results = [
+            {name: "Name", image: nick},
+            {name: "Name", image: nick},
+            {name: "Name", image: nick},
+            {name: "Name", image: nick},
+        ]
+
+        results.forEach((result) => {
+            html += `<div class="project-element"><img src="${result.image}"> <div class="name-container">${result.name}</div></div>`
+        })
+
+        return html
+    }
+
+    loadPageCount() {
+        let html = "";
+        let startPoint = this.currentPageCount
+        const maxPageCount = 10;
+        const maximumPageination = this.currentPageCount + 5
+        let maxNumber = maximumPageination <= maxPageCount ? maximumPageination : maxPageCount
+
+        if (startPoint === maxPageCount) {
+            startPoint -= 5
+            maxNumber++
+        }
+
+        for (let i = startPoint; i < maxNumber; i++) {
+            if (i !== this.currentPageCount)
+                html += `<div class="page-number">${i}</div>`
+            else
+                html += `<div class="page-number selected">${i}</div>`
+        }
+
+        if (maximumPageination <= maxPageCount)
+            html += `<div class="page-number">${maxPageCount}</div>`
 
         return html
     }
