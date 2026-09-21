3cbc71b66178f80a00d4d3cf1739be2431265c98
Joris van Rijn <jorisvanrijn@outlook.com>
2026-09-21 10:03:37 +0200

    gave not found own class

---
+++ src/javascript/PageLoaders/NotFound.js
@@ -0,0 +1,23 @@
+import { PageLoaderBase } from './PageLoaderBase.js'
+
+export class NotFound extends PageLoaderBase {
+    constructor(nav) {
+        super(nav);
+        this.currentPage = null
+    }
+
+  LoadPage () {
+    // load everything like listeners and page
+    document.querySelector('#app').innerHTML = `
+        ${this.header}
+                <div id="notfoundError">404 this page doesn't exist</div>
+        ${this.footer}
+    `
+
+      super.LoadPage()
+  }
+
+  UnloadPage () {
+    // remove page and unload listeners
+  }
+}
--- src/javascript/main.js
+++ src/javascript/main.js
@@ -2,30 +2,29 @@ import '../styling/style.css'
 import {NavHandler} from './classes/NavHandler.js'
 import {Aboutme} from './PageLoaders/AboutMe.js'
 import {Projects} from './PageLoaders/Projects.js'
+import {NotFound} from "./PageLoaders/NotFound.js";
 
 const Navigation = new NavHandler()
 const pages = {
     aboutme: Aboutme,
     myProjects: Projects,
-    // Contactme: contactPage
+    none: NotFound
 }
 
 function loadPage() {
     const currentPoint = Navigation.GetCurrentPoint()
+    let PageClass
 
-    if (pages[currentPoint]) {
-        const PageClass = pages[currentPoint]
-        const pageInstance = new PageClass(Navigation)
+    if (pages[currentPoint])
+        PageClass = pages[currentPoint]
+    else
+        PageClass = pages["none"]
 
-        pageInstance.LoadPage()
+    const pageInstance = new PageClass(Navigation)
 
-        return pageInstance
-    } else {
-        document.querySelector('#app').innerHTML = `
-        <div id="notfoundError">404 this page doesn't exist</div>
-    `
-        return false
-    }
+    pageInstance.LoadPage()
+
+    return pageInstance
 }
 
 loadPage();
