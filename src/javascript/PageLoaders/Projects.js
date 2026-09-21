33ffb44c614d47b4c7af543b4473b6b0425addf1
Joris van Rijn <jorisvanrijn@outlook.com>
2026-09-21 13:40:45 +0200

    added hover when hovering over projects

--- src/javascript/PageLoaders/Projects.js
+++ src/javascript/PageLoaders/Projects.js
@@ -1,4 +1,5 @@
-import { PageLoaderBase } from './PageLoaderBase.js'
+import {PageLoaderBase} from './PageLoaderBase.js'
+import nick from "../../assets/nick-widle-zootopia.gif"
 
 export class Projects extends PageLoaderBase {
     constructor(nav) {
@@ -6,14 +7,39 @@ export class Projects extends PageLoaderBase {
         this.currentPage = "myProjects"
     }
 
-  LoadPage () {
-    // load everything like listeners and page
-    document.querySelector('#app').innerHTML = `
+    LoadPage() {
+        // load everything like listeners and page
+        document.querySelector('#app').innerHTML = `
         ${this.header}
-        <div>MyPrjects</div>
+        <div>
+            <div>
+                <h1>
+                    Random selection
+                </h1>
+                <div id="random-selection">
+                    ${this.loadRandomProjects()}
+                </div>
+            </div>
+        </div>
         ${this.footer}
     `
 
-      super.LoadPage()
-  }
+        super.LoadPage()
+    }
+
+    loadRandomProjects() {
+        let html = "";
+        const results = [
+            {name: "Name", image: nick},
+            {name: "Name", image: nick},
+            {name: "Name", image: nick},
+            {name: "Name", image: nick},
+        ]
+
+        results.forEach((result) => {
+            html += `<div class="project-element"><img src="${result.image}"> <div class="name-container">${result.name}</div></div>`
+        })
+
+        return html
+    }
 }
